ecd6008e181dfdb9ac4b0122c358dc7d92290d7d
NJ-KIM <alwaysred1116@gmail.com>
2024-08-06 13:20:10 +0900

    0806am

---
+++ chapter03-method-and-api/src/main/java/com/ohgiraffers/section01/method/test.java
@@ -0,0 +1,7 @@
+package main.java.com.ohgiraffers.section01.method;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(Calculator.maxNumberOf(10,20));
+    }
+}
